3963d1c638ad8f274284126c58a2a8eae96b6a06
addonBrasil <repo@addonbrasil..tk>
2015-09-22 21:42:39 -0300

    new version

--- plugin.video.arquivobrasil/addon.py
+++ plugin.video.arquivobrasil/addon.py
@@ -4,7 +4,7 @@
 #####################################################################
 
 import urllib,urllib2,re,xbmcplugin, xbmcaddon, xbmcgui, time
-import client, jsunpack
+import urlresolver, client, jsunpack
 
 from BeautifulSoup import BeautifulSoup
 
@@ -16,6 +16,7 @@ icon      = addonfolder + '/icon.png'
 fanart    = addonfolder + '/fanart.jpg'
 
 base   = 'http://novelasgravadas.net/'
+basex  = 'http://tanoar.tv/'
 imgsrv = 'http://arquivobrasil.addonbrasil.tk/imgs/'
 
 #####################################################################
@@ -26,6 +27,23 @@ def menuPrincipal():
 		addDir('Jornalismo', base, 10, imgsrv + 'jornalismo.jpg')
 		addDir('Variedades', base, 10, imgsrv + 'variedades.jpg')
 		addDir('Esportes'  , base, 10, imgsrv + 'esportes.jpg')
+		addDir('Extras'    , base, 30, imgsrv + 'extras.jpg')
+
+		xbmcplugin.setContent(int(sys.argv[1]), 'movies')
+		xbmc.executebuiltin('Container.SetViewMode(500)')
+		
+def menuExtras():
+		addDir('Malhação 2015'    , basex + '?novos=1&e=malhacao-2015'                , 40, imgsrv + 'malhacao.png')
+		addDir('Hora 1 da Notícia', basex + '?novos=1&e=hora-1'                       , 40, imgsrv + 'hora-1.png')
+		addDir('Jornal Hoje'      , basex + '?novos=1&e=jornal-hoje'                  , 40, imgsrv + 'jornal-hoje.png')
+		addDir('Bem Estar'        , basex + '?novos=1&e=bem-estar'                    , 40, imgsrv + 'bem-estar.png')
+		addDir('É de Casa'        , basex + '?novos=1&e=e-de-casa'                    , 40, imgsrv + 'e-de-casa.png')
+		addDir('Encontro'         , basex + '?novos=1&e=encontro-com-fatima-bernardes', 40, imgsrv + 'encontro.png')
+		addDir('Esquenta'         , basex + '?novos=1&e=esquenta'                     , 40, imgsrv + 'esquenta.png')
+		addDir('Estrelas'         , basex + '?novos=1&e=estrelas'                     , 40, imgsrv + 'estrelas.png')
+		addDir('Programa do Gugu' , basex + '?novos=1&e=programa-do-gugu'             , 40, imgsrv + 'gugu.png')
+		addDir('Programa Raul Gil', basex + '?novos=1&e=programa-raul-gil'            , 40, imgsrv + 'raul-gil.png')
+		addDir('Video Show'       , basex + '?novos=1&e=video-show'                   , 40, imgsrv + 'video-show.png')
 
 		xbmcplugin.setContent(int(sys.argv[1]), 'movies')
 		xbmc.executebuiltin('Container.SetViewMode(500)')
@@ -42,6 +60,7 @@ def getListaCat(url, name):
 		elif name == 'Jornalismo' : temp = listaGeral[2]
 		elif name == 'Variedades' : temp = listaGeral[3]
 		elif name == 'Esportes'   : temp = listaGeral[4]
+		elif name == 'Extras'     : menuExtras()
 		
 		categorias = temp("li")
 		
@@ -82,7 +101,37 @@ def getVideosCat(url, name, iconimage):
 				
 		xbmcplugin.setContent(int(sys.argv[1]), 'movies')
 		xbmc.executebuiltin('Container.SetViewMode(500)')
-
+		
+def getVideosExt(url, name, iconimage):
+		link  = openURL(url)
+		soup  = BeautifulSoup(link)
+		
+		titulos = re.compile("title='(.*?)'").findall(link)
+		urls    = re.compile("Loadn\('(.*?)&").findall(link)
+		imgs    = re.compile("image:url\((.*?)\)").findall(link)
+		
+		i = 0
+		
+		for i in range(0,len(titulos)):
+			titE = titulos[i]
+			
+			urlE = basex + str(urls[i])
+			imgE = imgs[i]
+			
+			addDir(titE, urlE, 200, imgE, False)
+				
+		try :
+				paginas = re.compile('<b>.*?</b>, <a style=cursor:pointer onclick=Loadn\("(.*?)"').findall(link)
+				
+				for proxpag in paginas:
+						addDir('Próxima Página >>', basex + proxpag, 20, imgsrv + 'proxima.jpg')
+						break
+		except :
+				pass
+				
+		xbmcplugin.setContent(int(sys.argv[1]), 'movies')
+		xbmc.executebuiltin('Container.SetViewMode(500)')
+		
 def doPlay(url, name):
 		link = openURL(url)
 
@@ -125,6 +174,73 @@ def doPlay(url, name):
 				dialog = xbmcgui.Dialog()
 				dialog.ok("ARQUIVO BRASIL", "Video Indisponível", "Este vídeo ainda não esta disponível...", "Tente novamente em breve.")		
 				
+def doPlayExt(url, name):
+		link = openURL(url)
+
+		pg = 0
+		msgDialog = xbmcgui.DialogProgress()
+
+		msgDialog.create('ARQUIVO BRASIL', 'Abrindo Sinal', name, 'Por favor aguarde...')
+		msgDialog.update(25)
+		
+		playlist = xbmc.PlayList(xbmc.PLAYLIST_VIDEO)
+		playlist.clear()
+
+		urlExt = re.findall("<iframe src='(.*?)'", link)[0]
+		
+		linkExt = openURL(urlExt)
+		
+		msgDialog.update(50)
+		
+		if 'dailymotion' in linkExt :
+				urlDM = re.findall("src='http://www.dailymotion.com/(.*?)'", linkExt)[0]
+				urlDM = 'http://www.dailymotion.com/' + str(urlDM)
+
+				url2Play = getDmURL(urlDM)
+		else :
+				urlRSS = urlExt + '.rss'
+				
+				linkRSS = openURL(urlRSS)
+
+				url2Play = re.findall('<jwplayer:source file="(.*?)" />', linkRSS)		
+		
+		msgDialog.update(75)
+		
+		if url2Play:
+					liz = xbmcgui.ListItem(name, thumbnailImage=iconimage)
+					liz.setInfo('video', {'Title': name})
+					
+					if 'dailymotion' in url2Play :
+							liz.setPath(url2Play)
+							liz.setProperty('mimetype','video/mp4')
+							liz.setProperty('IsPlayable', 'true')
+							
+							playlist.add(url=url2Play, listitem=liz, index=7)
+					else :
+							
+							i = 0
+							
+							for i in range(len(url2Play)) :
+									urls2Play = url2Play[i].replace('./','http://dnshost.cf/player1/' )
+									
+									liz = xbmcgui.ListItem(name, thumbnailImage=iconimage)
+									liz.setInfo('video', {'Title': name})
+									liz.setPath(url)
+									liz.setProperty('mimetype','video/mp4')
+									liz.setProperty('IsPlayable', 'true')
+									
+									playlist.add(url=urls2Play, listitem=liz, index=7)
+									
+									i+=1
+						
+					msgDialog.update(100)
+				
+					xbmc.Player(xbmc.PLAYER_CORE_AUTO).play(playlist)
+		else:
+				msgDialog.update(100)
+				dialog = xbmcgui.Dialog()
+				dialog.ok("ARQUIVO BRASIL", "Video Indisponível", "Este vídeo ainda não esta disponível...", "Tente novamente em breve.")		
+		
 def getURL2Play(url):
     try:
         result = client.request(url, mobile=True, close=False)
@@ -162,6 +278,13 @@ def getURL2Play(url):
     except:
         return
 				
+				
+def getDmURL(vURL) :
+		urlVideo = urlresolver.HostedMediaFile(url=vURL).resolve()
+		
+		if urlVideo : return urlVideo
+		else        : return ''
+		
 def getImg(texto):
 		texto = texto.lower()
 		texto = texto.replace('ç','c').replace('ã','a').replace('õ','o')
@@ -241,11 +364,19 @@ except : pass
 try    : mode = int(params["mode"])
 except : pass
 
+#print "Mode : " + str(mode)
+#print "Icon : " + str(iconimage)
+#print "URL  : " + str(url)
+#print "Name : " + str(name)
+
 #####################################################################
 
 if   mode == None : menuPrincipal()
 elif mode == 10   : getListaCat(url, name)
 elif mode == 20   : getVideosCat(url,name, iconimage)
+elif mode == 30   : menuExtras()
+elif mode == 40   : getVideosExt(url,name, iconimage)
 elif mode == 100  : doPlay(url, name)
+elif mode == 200  : doPlayExt(url, name)
 	
 xbmcplugin.endOfDirectory(int(sys.argv[1]))
